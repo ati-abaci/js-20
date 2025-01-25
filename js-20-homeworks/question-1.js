e2304f800816672e8be7da5a06d96a2b63e2f4f8
Ati abbasi <atiabaci8183@gmail.com>
2025-01-25 18:38:56 +0330

    question 1 done

---
+++ js-20-homeworks/question-1.js
@@ -0,0 +1,8 @@
+/*
+   what is the difference between an asynchronous function and a synchronous function?
+*/
+
+// ! Answer
+//synchronous functions: are straightforward but can lead to performance issues in applications that require responsiveness.
+
+//Asynchronous functions: on the other hand, provide a way to handle time-consuming operations without blocking the execution of other code, making them essential for modern web development and applications that require high performance.
